2f94e5851fbc8022aa12cbdaaf7fa8668f3349f1
yumemiru-masomi <yumemi.saito@blueish.co.jp>
2024-10-09 16:53:01 +0900

    fix: ディスコードの認証をGoogleに変更

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -5,9 +5,8 @@ import {
   type NextAuthOptions,
 } from "next-auth";
 import { type Adapter } from "next-auth/adapters";
-import DiscordProvider from "next-auth/providers/discord";
+import GoogleProvider from "next-auth/providers/google";
 
-import { env } from "~/env";
 import { db } from "~/server/db";
 
 /**
@@ -48,9 +47,9 @@ export const authOptions: NextAuthOptions = {
   },
   adapter: PrismaAdapter(db) as Adapter,
   providers: [
-    DiscordProvider({
-      clientId: env.DISCORD_CLIENT_ID,
-      clientSecret: env.DISCORD_CLIENT_SECRET,
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID ?? "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
     }),
     /**
      * ...add more providers here.
